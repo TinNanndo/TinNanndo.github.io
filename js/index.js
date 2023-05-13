4a793afd4606ff04356cbedfe8b178e735a7b0c4
TinNanndo <tin.jovanovic003@gmail.com>
2023-05-13 16:27:13 +0200

    a

--- js/index.js
+++ js/index.js
@@ -4,6 +4,48 @@ const toggleMenu = () => {
     document.body.classList.toggle("open");
 };
 
+// WAYPOINT
+
+$(document).ready(function() {
+  $('.js--features').waypoint(function(direction) {
+    console.log('waypoint called'); // add this to check if waypoint is being called
+    if (direction == 'down') {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '160px'
+  });
+});
+
+// SCROLL ACTIVE
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var scrollPosition = $(this).scrollTop();
+    console.log('scrollPosition: ' + scrollPosition);
+    
+    $('section').each(function() {
+      var sectionTop = $(this).offset().top;
+      var sectionBottom = sectionTop + $(this).outerHeight();
+      console.log('sectionTop: ' + sectionTop + ', sectionBottom: ' + sectionBottom);
+      
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        var navItemId = $(this).attr('id') + '-link';
+        console.log('highlighting ' + navItemId);
+        $('#' + navItemId).addClass('active');
+      } else {
+        var navItemId = $(this).attr('id') + '-link';
+        console.log('unhighlighting ' + navItemId);
+        $('#' + navItemId).removeClass('active');
+      }
+    });
+  });
+});
+
+
+
 // SCROLL
 
 const sections = document.querySelectorAll("section[id]");
@@ -31,4 +73,4 @@ function navHighlighter() {
   });
 }
 
-// WAYPOINT
+$('body').scrollspy({ target: '.navbar-menu' })
