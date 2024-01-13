45506f2a03cfeb7fe38cd65bf3f3632305ee1f49
TinNanndo <tin.jovanovic003@gmail.com>
2024-01-13 19:39:13 +0100

    a

--- html/druga stranica/js/index.js
+++ html/druga stranica/js/index.js
@@ -21,31 +21,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Mail
 
-async function submitForm() {
-    try {
-        var email = document.getElementById('email').value;
+// async function submitForm() {
+//     try {
+//         var email = document.getElementById('email').value;
 
-        if (!email.toString().includes("@")) return;
-        const response = await fetch('https://api.firmus.hr/waitlist', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: email
-            }),
-        });
+//         if (!email.toString().includes("@")) return;
+//         const response = await fetch('https://api.firmus.hr/waitlist', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({
+//                 email: email
+//             }),
+//         });
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        document.getElementById('successMessage').style.display = 'block';
-         document.getElementById('myForm').style.display = 'none';
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+//         document.getElementById('successMessage').style.display = 'block';
+//          document.getElementById('myForm').style.display = 'none';
 
-        // Handle successful response here if needed
-        console.log('Form submitted successfully');
-    } catch (error) {
-        // Handle errors here
-        console.error('There was a problem with the form submission:', error);
-    }
-}
+//         // Handle successful response here if needed
+//         console.log('Form submitted successfully');
+//     } catch (error) {
+//         // Handle errors here
+//         console.error('There was a problem with the form submission:', error);
+//     }
+// }
