1c2399d1e02e9d1c16a1b13f02b79b9acd5b531f
TinNanndo <tin.jovanovic003@gmail.com>
2024-10-19 14:47:29 +0200

    a pokusavan animacije al san glup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { motion } from 'framer-motion';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -23,11 +24,16 @@ const Header = () => {
     };
   }, [menuOpen]);
 
+  const menuVariants = {
+    hidden: { opacity: 0, x: '-100%' },
+    visible: { opacity: 1, x: 0 },
+  };
+
   return (
     <>
       <nav className="bg-customOrange border-b-8 border-customYellow rounded-t-md flex flex-col md:flex-row items-center justify-between md:justify-start">
         <div className="flex items-center justify-between w-full md:w-auto">
-          <div className="text-4xl p-4 text-customWhite hover:bg-customYellow rounded-tl-md">
+          <div className="text-4xl p-4 text-customWhite md:hover:bg-customYellow rounded-tl-md">
             <FontAwesomeIcon icon={faHouse} />
           </div>
           <div className="md:hidden text-4xl p-4 text-customWhite z-50" onClick={toggleMenu}>
@@ -35,54 +41,99 @@ const Header = () => {
           </div>
         </div>
 
-        <div
-          className={`flex-col md:flex-row md:flex items-center w-full md:w-auto absolute top-0 left-0 h-full bg-customOrange z-40 transition-transform transform ${
+        <motion.div
+          className={`flex-col md:flex-row md:flex items-center w-full md:w-auto ${
             menuOpen ? 'flex' : 'hidden'
-          } md:static md:flex md:h-auto`}
+          } md:flex md:static md:h-auto absolute top-0 left-0 h-full bg-customOrange z-40 transition-transform transform`}
+          initial="hidden"
+          animate={menuOpen ? 'visible' : 'hidden'}
+          variants={menuVariants}
+          transition={{ duration: 0.5 }}
         >
           <div className='relative top-32 right-16 md:static md:top-0 md:right-0 flex flex-col md:flex-row'>
-          <div className="w-full md:w-auto">
-            <a
-              href="#about"
-              className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
-              onClick={() => setMenuOpen(false)}  // Close menu when clicked
-            >
-              about
-            </a>
-          </div>
+            <div className="w-full md:w-auto">
+              <a
+                href="#about"
+                className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
+                onClick={() => setMenuOpen(false)}  // Close menu when clicked
+              >
+                about
+              </a>
+            </div>
 
-          <div className="w-full md:w-auto">
-            <a
-              href="#projects"
-              className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
-              onClick={() => setMenuOpen(false)}  // Close menu when clicked
-            >
-              projects
-            </a>
-          </div>
+            <div className="w-full md:w-auto">
+              <a
+                href="#projects"
+                className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
+                onClick={() => setMenuOpen(false)}  // Close menu when clicked
+              >
+                projects
+              </a>
+            </div>
 
-          <div className="w-full md:w-auto">
-            <a
-              href="#contact"
-              className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
-              onClick={() => setMenuOpen(false)}  // Close menu when clicked
-            >
-              contact
-            </a>
+            <div className="w-full md:w-auto">
+              <a
+                href="#contact"
+                className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
+                onClick={() => setMenuOpen(false)}  // Close menu when clicked
+              >
+                contact
+              </a>
+            </div>
           </div>
-          </div>
-        </div>
+        </motion.div>
       </nav>
 
       <header className="p-4 h-screen flex flex-col md:flex-row items-center justify-center space-y-8 md:space-y-0 md:space-x-80">
         <div className="flex flex-col items-center md:items-start">
-          <h1 className="text-4xl md:text-8xl font-bold text-customBlack">Hello!</h1>
-          <h1 className="text-4xl md:text-8xl font-bold text-customBlack">I'm Tin.</h1>
-          <span></span>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delayChildren: 0.5,
+                  staggerChildren: 0.3,
+                },
+              },
+            }}
+          >
+            <motion.h1
+              className="text-4xl md:text-8xl font-bold text-customBlack"
+              variants={{
+                hidden: { opacity: 0, y: 50 },
+                visible: { opacity: 1, y: 0 },
+              }}
+            >
+              Hello!
+            </motion.h1>
+            <motion.h1
+              className="text-4xl md:text-8xl font-bold text-customBlack"
+              variants={{
+                hidden: { opacity: 0, y: 50 },
+                visible: { opacity: 1, y: 0 },
+              }}
+            >
+              I'm Tin.
+            </motion.h1>
+          </motion.div>
         </div>
 
         <div className="flex justify-center items-center">
-          <div className="bg-customBlack h-48 w-48 md:h-96 md:w-96 rounded-full"></div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.5,
+              delay: 0.5,
+              ease: [0.48, 0.15, 0.25, 0.96],
+            }}
+          >
+            <div className="bg-customBlack h-48 w-48 md:h-96 md:w-96 rounded-full"></div>
+          </motion.div>
         </div>
       </header>
     </>
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../css/Loading.css'; // Import the CSS file for custom animations
 
 const Loading = () => (
   <div className="flex flex-col items-center justify-center h-screen bg-customYellow">
-    <div className="border-4 border-t-4 border-customOrange border-customBlack rounded-full w-10 h-10 animate-spin"></div>
-    <p className="mt-2 text-lg text-customWhite">Loading...</p>
+    <div className="container">
+      <div className="cube"><div className="cube__inner"></div></div>
+      <div className="cube"><div className="cube__inner"></div></div>
+      <div className="cube"><div className="cube__inner"></div></div>
+    </div>
   </div>
 );
 
