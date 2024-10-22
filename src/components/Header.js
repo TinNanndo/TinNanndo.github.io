36d20b5a1d07b23b4aed1f7cc3deeb63e6fbed37
TinNanndo <tin.jovanovic003@gmail.com>
2024-10-22 17:11:16 +0200

    responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,21 +5,18 @@ import { AnimatePresence, motion } from 'framer-motion';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 767);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  // Disable scroll when menu is open
   useEffect(() => {
     if (menuOpen) {
       document.body.classList.add('overflow-hidden');
     } else {
       document.body.classList.remove('overflow-hidden');
     }
-
-    // Cleanup function to remove overflow-hidden when the component is unmounted or menu is closed
     return () => {
       document.body.classList.remove('overflow-hidden');
     };
@@ -27,12 +24,18 @@ const Header = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
+      const isNowMobile = window.innerWidth <= 767;
+      setIsMobile(isNowMobile);
+      if (!isNowMobile) {
+        setMenuOpen(false);
+      }
     };
 
     window.addEventListener('resize', handleResize);
-    return () => {window.removeEventListener('resize', handleResize)};
-  })
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const menuVariants = {
     hidden: { opacity: 0, x: '-100%' },
@@ -53,54 +56,54 @@ const Header = () => {
         </div>
 
         <AnimatePresence>
-        {menuOpen && (
-        <motion.div
-          className={`flex-col md:flex-row md:flex items-center w-full md:w-auto ${
-            menuOpen ? 'flex' : 'hidden'
-          } md:flex md:static md:h-auto absolute top-0 left-0 h-full bg-customOrange z-40 transition-transform transform`}
-          initial="hidden"
-          animate={menuOpen ? 'visible' : 'hidden'}
-          exit = "exit"
-          variants={ isMobile ? menuVariants : {} }
-          transition={{ duration: 0.3 }}
-        >
-          <div className='relative top-32 right-16 md:static md:top-0 md:right-0 flex flex-col md:flex-row'>
-            <div className="w-full md:w-auto">
-              <a
-                href="#about"
-                className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
-                onClick={() => setMenuOpen(false)}  // Close menu when clicked
-              >
-                about
-              </a>
-            </div>
+          {(menuOpen || !isMobile) && (
+            <motion.div
+              className={`flex-col md:flex-row md:flex items-center w-full md:w-auto ${
+                menuOpen || !isMobile ? 'flex' : 'hidden'
+              } md:flex md:static md:h-auto absolute top-0 left-0 h-full bg-customOrange z-40 transition-transform transform`}
+              initial="hidden"
+              animate={menuOpen || !isMobile ? 'visible' : 'hidden'}
+              exit="exit"
+              variants={isMobile ? menuVariants : {}}
+              transition={{ duration: 0.3 }}
+            >
+              <div className='relative top-32 right-16 md:static md:top-0 md:right-0 flex flex-col md:flex-row'>
+                <div className="w-full md:w-auto">
+                  <a
+                    href="#about"
+                    className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    about
+                  </a>
+                </div>
 
-            <div className="w-full md:w-auto">
-              <a
-                href="#projects"
-                className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
-                onClick={() => setMenuOpen(false)}  // Close menu when clicked
-              >
-                projects
-              </a>
-            </div>
+                <div className="w-full md:w-auto">
+                  <a
+                    href="#projects"
+                    className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    projects
+                  </a>
+                </div>
 
-            <div className="w-full md:w-auto">
-              <a
-                href="#contact"
-                className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
-                onClick={() => setMenuOpen(false)}  // Close menu when clicked
-              >
-                contact
-              </a>
-            </div>
-          </div>
-        </motion.div>
-        )}
+                <div className="w-full md:w-auto">
+                  <a
+                    href="#contact"
+                    className="block text-4xl p-4 font-bold text-customWhite md:hover:bg-customYellow"
+                    onClick={() => setMenuOpen(false)}
+                  >
+                    contact
+                  </a>
+                </div>
+              </div>
+            </motion.div>
+          )}
         </AnimatePresence>
       </nav>
 
-      <header className="p-4 h-screen flex flex-col md:flex-row items-center justify-center space-y-8 md:space-y-0 md:space-x-80">
+      <header className="p-4 h-screen flex flex-col md:flex-row items-center justify-center space-y-8 md:space-y-0 md:space-x-20 lg:space-x-40 xl:space-x-64">
         <div className="flex flex-col items-center md:items-start">
           <motion.div
             initial="hidden"
@@ -118,7 +121,7 @@ const Header = () => {
             }}
           >
             <motion.h1
-              className="text-4xl md:text-8xl font-bold text-customBlack"
+              className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold text-customBlack text-center md:text-left"
               variants={{
                 hidden: { opacity: 0, y: 50 },
                 visible: { opacity: 1, y: 0 },
@@ -127,7 +130,7 @@ const Header = () => {
               Hello!
             </motion.h1>
             <motion.h1
-              className="text-4xl md:text-8xl font-bold text-customBlack"
+              className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold text-customBlack text-center md:text-left md:relative md:left-2 xl:left-8"
               variants={{
                 hidden: { opacity: 0, y: 50 },
                 visible: { opacity: 1, y: 0 },
@@ -135,6 +138,16 @@ const Header = () => {
             >
               I'm Tin.
             </motion.h1>
+
+            <motion.span
+              className='text-lg md:text-lg xl:text-xl text-customBlack text-center md:text-left'
+              variants={{
+                hidden: { opacity: 0, y: 50 },
+                visible: { opacity: 1, y: 0 },
+              }}
+            >
+              I'm a passionate junior web designer with strong <br />focus on front-end development and UI/UX design.
+            </motion.span>
           </motion.div>
         </div>
 
@@ -148,7 +161,7 @@ const Header = () => {
               ease: [0.48, 0.15, 0.25, 0.96],
             }}
           >
-            <div className="bg-customBlack h-48 w-48 md:h-96 md:w-96 rounded-full"></div>
+            <div className="bg-customBlack h-40 w-40 sm:h-48 sm:w-48 md:h-64 md:w-64 lg:h-80 lg:w-80 rounded-full"></div>
           </motion.div>
         </div>
       </header>
